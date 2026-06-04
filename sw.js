2c4ebed7447f984abbb7d3e260d2c2acb1fe3637
Grzegosz Mielniczuk <wojciechmielniczuk78@gmail.com>
2026-06-04 21:40:45 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,30 @@
-const CACHE='vaultlink-v1';
-const ASSETS=['/index.html','/login.html','/app.html','/pricing.html','/manifest.json'];
+const CACHE = 'vaultlink-v2';
+const ASSETS = [
+  '/index.html',
+  '/login.html',
+  '/app.html',
+  '/pricing.html',
+  '/manifest.json',
+  '/logo.png'
+];
 
-self.addEventListener('install',e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
-  self.skipWaiting();
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
 });
 
-self.addEventListener('activate',e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
-  self.clients.claim();
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
 });
 
-self.addEventListener('fetch',e=>{
-  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
 });
